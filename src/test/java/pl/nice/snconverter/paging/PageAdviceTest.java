07a330df220120621042089932bc6c8d03374941
Piotr Głowacki <pege28@wp.pl>
2021-05-03 14:21:57 +0200

    Created PageAdvice tests

---
+++ src/test/java/pl/nice/snconverter/paging/PageAdviceTest.java
@@ -0,0 +1,62 @@
+package pl.nice.snconverter.paging;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.servlet.http.HttpServletRequest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PageAdviceTest {
+    @Autowired
+    private PageAdvice pageAdvice;
+    @Autowired
+    private HttpServletRequest request;
+
+   @Test
+    void shouldReturnLinkWithPageNumberEqualToTwo() {
+        //given
+        long totalPages = 10;
+        int page = 1;
+        //when
+        String result = pageAdvice.getNextPageURL(totalPages, page, request);
+        //then
+        assertEquals("http://localhost?page=2", result);
+    }
+
+    @Test
+    void shouldReturnNull() {
+       //given
+        long totalPages = 10;
+        int page = 10;
+        //when
+        String result = pageAdvice.getNextPageURL(totalPages, page, request);
+        //then
+        assertNull(result);
+    }
+
+    @Test
+    void shouldReturnNumbersOfTotalPagesEqualToTen() {
+       //given
+        int totalRecords = 100;
+        pageAdvice.setRecordsPerPage(10);
+        //when
+        int result = pageAdvice.getTotalPages(totalRecords);
+        //then
+        assertEquals(10, result);
+    }
+
+    @Test
+    void shouldReturnNumbersOfTotalPagesEqualToEleven() {
+        //given
+        int totalRecords = 101;
+        pageAdvice.setRecordsPerPage(10);
+        //when
+        int result = pageAdvice.getTotalPages(totalRecords);
+        //then
+        assertEquals(11, result);
+    }
+}
